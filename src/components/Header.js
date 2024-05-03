da1ba6d1293323548c7fd267d707208d20ee63ef
lynnelle <lynnellewanja37@mail.com>
2024-05-03 12:14:08 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Header ({ isDarkMode, onDarkModeClick }) {
+    return (
+        <header>
+          <button onClick={onDarkModeClick}>{isDarkMode ? "Dark" : "Light"} Mode</button>
+        </header>
+      );
+    }
+    
+export default Header;
